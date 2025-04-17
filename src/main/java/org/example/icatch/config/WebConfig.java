42c26b866752d8b3b0971509d64bcf7f84841e8d
k-sulim <joy06270@naver.com>
2025-04-17 23:06:23 +0900

    cors추가

---
+++ src/main/java/org/example/icatch/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.example.icatch.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // 실제 환경에서는 특정 도메인만 허용하는 것이 좋습니다
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
